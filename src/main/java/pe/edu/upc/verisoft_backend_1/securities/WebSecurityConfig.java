325cc89143937bcee608c942900ca1dd75a91fee
1Iluu <iguana29x@gmail.com>
2024-06-13 17:03:06 -0500

    update websecuruty

--- src/main/java/pe/edu/upc/verisoft_backend_1/securities/WebSecurityConfig.java
+++ src/main/java/pe/edu/upc/verisoft_backend_1/securities/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig implements WebMvcConfigurer {
 
 
 
-    private static final String[] WHITE_LIST_URL = { "/public/**","/api/public/**","/seguros","/seguros/{id}","/users","/roles","/cirugias"};
+    private static final String[] WHITE_LIST_URL = { "/public/**","/api/public/**","/seguros","/seguros/{id}","/users","/roles","/cirugias","/especialidades","/especialidades/{id}"};
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
